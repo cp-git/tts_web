c69755f62802c18733bfa20955c964483faca298
AkashD <akash.d@cloudpointamerica.com>
2023-08-02 16:55:49 +0530

    employeeUI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompanyComponent } from './company/components/company/company.component';
-
+import { EmployeeComponent } from './employee/components/employee/employee.component';
+import { CreateEmployeeComponent } from './employee/components/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './employee/components/update-employee/update-employee.component';
 const routes: Routes = [
-  { path: 'company', component: CompanyComponent }
+  { path: 'company', component: CompanyComponent },
+  {path:'employee',component:EmployeeComponent},
+  {path:'createEmp',component:CreateEmployeeComponent},
+  {path:'updateEmp',component:UpdateEmployeeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyModule } from './company/company.module';
-
+import { EmployeeModule } from './employee/employee.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
-    CompanyModule
+HttpClientModule,
+    CompanyModule,
+    EmployeeModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
