f8faf6a7510f0eada69714b12498a7af8036cd1d
Kaushik <kaushikwalke6565@gmail.com>
2023-12-20 11:55:53 +0530

    commit

--- tts_web/src/app/jobportal/services/jobportal.service.ts
+++ tts_web/src/app/jobportal/services/jobportal.service.ts
@@ -10,27 +10,42 @@ export class JobportalService {
     {
       portalId: 1,
       portalName: 'LinkedIn',
-      portalDescription: 'from linkedin'
+      portalDescription: 'from linkedin',
+      companyId: 1,
+      forBench: true,
+      forSourcing: false,
     },
     {
       portalId: 2,
       portalName: 'Naukari.com',
-      portalDescription: 'from naukari.com'
+      portalDescription: 'from naukari.com',
+      companyId: 1,
+      forBench: true,
+      forSourcing: false,
     },
     {
       portalId: 3,
       portalName: 'Indeed',
-      portalDescription: 'from indeed'
+      portalDescription: 'from indeed',
+      companyId: 1,
+      forBench: true,
+      forSourcing: false,
     },
     {
       portalId: 4,
       portalName: 'Glassdoor',
-      portalDescription: 'from Glassdoor'
+      portalDescription: 'from Glassdoor',
+      companyId: 1,
+      forBench: true,
+      forSourcing: false,
     },
     {
       portalId: 5,
       portalName: 'Upword',
-      portalDescription: 'from upword'
+      portalDescription: 'from upword',
+      companyId: 1,
+      forBench: true,
+      forSourcing: false,
     },
     // Add more data as needed
   ];
